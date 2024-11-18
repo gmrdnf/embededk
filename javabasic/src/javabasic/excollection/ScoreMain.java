bd1627cb1cee236b34c96c1c5832624b3c920702
Administrator <Administrator@DESKTOP-HUJUVSS>
2024-11-18 17:46:24 +0900

    xas

--- javabasic/src/javabasic/excollection/ScoreMain.java
+++ javabasic/src/javabasic/excollection/ScoreMain.java
@@ -1,12 +1,8 @@
 package javabasic.excollection;
 
 import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.Iterator;
 import java.util.List;
-import java.util.Map;
 import java.util.Scanner;
-import java.util.Set;
 import java.util.StringTokenizer;
 
 public class ScoreMain {
