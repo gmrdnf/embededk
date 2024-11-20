f834c1e5ad7bb5f83a7a607c5b612c3a3829bc29
Administrator <Administrator@DESKTOP-HUJUVSS>
2024-11-20 17:43:21 +0900

    ㅁㄴㅇ

--- javabasic/src/javabasic/excollection/Card.java
+++ javabasic/src/javabasic/excollection/Card.java
@@ -1,15 +1,12 @@
 package javabasic.excollection;
 
 public class Card {
-	// 숫자와 무늬를 가지고있음 카드는
-	// 2부터 A까지 JQK ( 11,12,13)
-	// number
-	// shape
-	private Integer num;//숫자(A~10,J,Q,K)1~13
-	private String shape;//무늬(스페이드,다이아,하트,클로버
-
+	
+	private Integer num;	// 숫자 (A~10, J, Q, K)
+	private String shape;	// 무늬 (스페이드, 다이아몬드, 하트, 클로버)
+	
 	public Card() {
-	};
+	}
 
 	public Card(Integer num, String shape) {
 		super();
@@ -32,10 +29,23 @@ public class Card {
 	public void setShape(String shape) {
 		this.shape = shape;
 	}
-
+	
 	@Override
 	public String toString() {
 		return shape+num;
 	}
 	
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
--- javabasic/src/javabasic/excollection/PokerMain.java
+++ javabasic/src/javabasic/excollection/PokerMain.java
@@ -1,46 +1,112 @@
 package javabasic.excollection;
 
 import java.util.ArrayList;
-import java.util.Collection;
 import java.util.Collections;
-import java.util.Iterator;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class PokerMain {
-	// 플레이어 5명만들기/
-	// 카드 52장 만든다 무늬당 13장씩 2~14
-	public static void main(String[] args) {
 
+	public static void main(String[] args) {
+		
+		// 플레이어를 5명 만든다
 		List<Player> playerList = new ArrayList<Player>();
 		playerList.add(new Player("홍길동", Collections.EMPTY_LIST));
 		playerList.add(new Player("이순신", Collections.EMPTY_LIST));
 		playerList.add(new Player("강감찬", Collections.EMPTY_LIST));
 		playerList.add(new Player("권율", Collections.EMPTY_LIST));
 		playerList.add(new Player("최영", Collections.EMPTY_LIST));
-
-		// (S2~S14,D2~D14,H2~H14,C2~C14)
-		List<Card> totalCardList = new ArrayList<Card>();
-		List<String>shapeList =new ArrayList<String>();
-		shapeList.add("S");	shapeList.add("D");	shapeList.add("H");	shapeList.add("C");
 		
-		for (int i = 0; i < shapeList.size(); i++) {
-			for (int j = 2; j < 15; j++) {
-				totalCardList.add(new Card(j,shapeList.get(i)));
+		// 카드를 52장 만든다 (S2~S14, D2~D14, H2~H14, C2~C14)
+		List<Card> totalCardList = new ArrayList<Card>();
+		
+		List<String> shapeList = new ArrayList<String>();
+		shapeList.add("S"); shapeList.add("D");
+		shapeList.add("H"); shapeList.add("C"); 
+		
+		for (int i=0; i<shapeList.size(); i++) {
+			for (int j=2; j<15; j++) {
+				totalCardList.add(new Card(j, shapeList.get(i)));
 			}
 		}
 		System.out.println(totalCardList);
 		
-		//카드를 섞자
-		Collections.shuffle(totalCardList); 
+		// 카드를 섞자
+		Collections.shuffle(totalCardList);
 		System.out.println(totalCardList);
-		//플레이어들에게 7장씩 나눠준다
-		//List 인터페이스에 List<E>subList(int fromIndex,int toIndex)메소드
-		for (int i = 0; i < playerList.size(); i++) {
+		
+		// 플레이어들에게 7장씩 나눠준다
+		// List 인터페이스에 List<E> subList(int fromIndex, int toIndex) 메소드
+		for (int i=0; i<playerList.size(); i++) {
 			playerList.get(i).setCardList(totalCardList.subList(i*7, (i*7)+7));
 		}
-		for (Player player : playerList) {
-			System.out.println(player);
+		
+		// 플레이어의 숫자카운트맵, 무늬카운트맵을 초기화하고 값을 설정
+		for (int i=0; i<playerList.size(); i++) {
+			
+			// 플레이어의 숫자카운트맵, 무늬카운트맵을 생성
+			Player player = playerList.get(i);
+			player.setNumCountMap(new HashMap<Integer, Integer>());
+			player.setShapeCountMap(new HashMap<String, Integer>());
+			
+			// 플레이어의 숫자카운트맵, 무늬카운트맵을 초기화
+			initMap(player.getNumCountMap(), player.getShapeCountMap());
+			
+			// 카드의 숫자, 무늬 카운팅
+			for (Card card : player.getCardList()) {
+				player.getNumCountMap().put(card.getNum(), 
+						player.getNumCountMap().get(card.getNum()) + 1);
+				player.getShapeCountMap().put(card.getShape(), 
+						player.getShapeCountMap().get(card.getShape()) + 1);
+			}
 		}
 		
+		for (Player player : playerList) {
+			System.out.println(player);
+			printResult(player);
+		}
+		
+	} // main
+	
+	// 플레이어의 숫자카운트맵, 무늬카운트맵을 초기화 메소드
+	private static void initMap(Map<Integer, Integer> numCountMap, 
+			Map<String, Integer> shapeCountMap) {
+		for (int i=2; i<15; i++) {
+			numCountMap.put(i, 0);
+		}
+		shapeCountMap.put("S", 0);
+		shapeCountMap.put("D", 0);
+		shapeCountMap.put("H", 0);
+		shapeCountMap.put("C", 0);
 	}
-}
+	
+	private static void printResult(Player player) {
+		String resultStr = "";
+		if (PokerRule.isRoyalStraightFlush(player)) resultStr = "[로열스트레이트플러시]";
+		else if (PokerRule.isStraightFlush(player)) resultStr = "[스트레이트플러시]";
+		else if (PokerRule.isFourCard(player)) resultStr = "[포카드]";
+		else if (PokerRule.isFullHouse(player)) resultStr = "[풀하우스]";
+		else if (PokerRule.isFlush(player)) resultStr = "[플러시]";
+		else if (PokerRule.isStraight(player)) resultStr = "[스트레이트]";
+		else if (PokerRule.isTriple(player)) resultStr = "[트리플]";
+		else if (PokerRule.isTwoPair(player)) resultStr = "[투 페어]";
+		else if (PokerRule.isOnePair(player)) resultStr = "[원 페어]";
+		else resultStr = "[족보 없음]";
+		System.out.println(resultStr);
+	}
+	
+} // class
+
+
+
+
+
+
+
+
+
+
+
+
+
